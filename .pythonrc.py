78d75834257c1da4642ea7ac54277c585c9bd62e
João Faria <joao.faria@astro.up.pt>
2018-11-29 11:20:20 +0000

    Small changes

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,16 +1,19 @@
 import io
 import matplotlib.pyplot as plt
 
-donothing = False
-
+replace_figure = True
 try:
     from PySide.QtGui import QApplication, QImage
 except ImportError:
     try:
-        from PyQt5.QtWidgets import QApplication
-        from PyQt5.QtGui import QImage
+        from PyQt4.QtGui import QApplication, QImage
     except ImportError:
-        donothing = True
+        try:
+            from PyQt5.QtWidgets import QApplication
+            from PyQt5.QtGui import QImage
+        except ImportError:
+            replace_figure = False
+
 
 def add_clipboard_to_figures():
     # use monkey-patching to replace the original plt.figure() function with
@@ -19,6 +22,7 @@ def add_clipboard_to_figures():
 
     def newfig(*args, **kwargs):
         fig = oldfig(*args, **kwargs)
+
         def clipboard_handler(event):
             if event.key == 'ctrl+c':
                 # store the image in a buffer using savefig(), this has the
@@ -27,14 +31,17 @@ def add_clipboard_to_figures():
                 # grab the canvas using Qt
                 buf = io.BytesIO()
                 fig.savefig(buf)
-                QApplication.clipboard().setImage(QImage.fromData(buf.getvalue()))
+                QApplication.clipboard().setImage(
+                    QImage.fromData(buf.getvalue()))
                 buf.close()
                 print('Image copied to clipboard.')
+
         fig.canvas.mpl_connect('key_press_event', clipboard_handler)
         return fig
 
     plt.figure = newfig
 
-if not donothing:
+
+if replace_figure:
     # print('replacing plt.figure')
     add_clipboard_to_figures()
